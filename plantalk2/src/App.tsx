bc552dc5afec133f01ca8c253b2bdf4c77a61980
masa <massanndesu52@gmail.com>
2025-03-17 15:25:17 +0900

    change: 細かい修正

--- plantalk2/src/App.tsx
+++ plantalk2/src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import Login from './Login'
+import Login from './login'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Title from './components/title';
+import Title from './Title';
 
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-    <div className="title-screen">
         <Routes>
           <Route path="/" element={<Title />} />
           <Route path="/login" element={<Login />} />
         </Routes>
-      </div>
     </BrowserRouter>
   )
 }
