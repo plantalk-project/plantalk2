e486a642634b5ecb8c77b691d83a944bed05e7f5
tanaka00005 <x23054xx@aitech.ac.jp>
2025-03-31 14:14:46 +0900

    スマホとの接続

--- plantalk2/vite.config.ts
+++ plantalk2/vite.config.ts
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // これを追加！
+    port: 5174,       // 正しいポートを指定
+    strictPort: true,
+    allowedHosts: ["*"], 
+  },
   optimizeDeps: {
+    
     include: ["swiper"], // ここに 'swiper' を追加
   },
 })
